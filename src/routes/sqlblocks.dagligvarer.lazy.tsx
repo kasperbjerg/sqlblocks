bd3b7efcca216724eb3acf46cbe6c036d639ff6b
Kasper Bjerg <kasperbjerg@kasperbjerg.com>
2024-02-25 17:47:39 +0100

    two diffent assignments with feedback--- proof of concept

--- src/routes/sqlblocks.dagligvarer.lazy.tsx
+++ src/routes/sqlblocks.dagligvarer.lazy.tsx
@@ -1,10 +1,9 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
 import logo from '/logo.svg';
-import React from 'react';
+import React, { useState } from 'react';
 
-import BlocklyComponent, { Block, Value, Field, Shadow } from '../Blockly';
-import '../blocks/customblocks';
-import '../generator/generator';
+import Bogliste from '../Blockly/Bogliste.jsx';
+import Dagligvarer from '../Blockly/Dagligvarer.jsx';
 
 export const Route = createLazyFileRoute('/sqlblocks/dagligvarer')({
   component: About,
@@ -13,33 +12,7 @@ export const Route = createLazyFileRoute('/sqlblocks/dagligvarer')({
 function About() {
   return (
     <>
-      <div className="relative top-2 col-span-4">
-        <p className="text-1xl">Her kan der stå opgaver</p>
-        <BlocklyComponent
-          readOnly={false}
-          trashcan={false}
-          media={'media/'}
-          move={{
-            scrollbars: {
-              horizontal: false,
-              vertical: false,
-            },
-            drag: true,
-            wheel: true,
-          }}
-          initialXml={`
-              <xml xmlns="http://www.w3.org/1999/xhtml">
-                <Block type="run_sqlblocks" />
-              </xml>
-          `}
-        >
-          <Block type="create_table" />
-          <Block type="add_integer_column" />
-          <Block type="add_text_column" />
-          <Block type="insert_into" />
-          <Block type="select_*" />
-        </BlocklyComponent>
-      </div>
+      <Dagligvarer />
     </>
   );
 }

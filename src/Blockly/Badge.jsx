79c646fb0737bd1b5b29d4326df287ff3297b819
Kasper Bjerg <kasperbjerg@kasperbjerg.com>
2024-03-04 14:58:10 +0100

    badges directory agian

--- src/Blockly/Badge.jsx
+++ src/Blockly/Badge.jsx
@@ -5,7 +5,7 @@ export default function Badge({ badgeName, conditionsArray }) {
     <>
       {conditionsArray.every((key) => key) ? (
         <div className="transition duration-[3000ms] ease-out hover:scale-[4] hover:animate-pulse">
-          <img src={'/badges/' + badgeName} className="h-8" alt="Badge" />
+          <img src={'/sqlblocks/' + badgeName} className="h-8" alt="Badge" />
         </div>
       ) : (
         <div></div>
